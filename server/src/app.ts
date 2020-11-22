1b44693620e0664d40c515e79c92eb856ab55888
VitorRedfox <vitor_pereira.2013@hotmail.com>
2020-11-22 13:57:06 -0300

    feat(server): add ssl config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,43 @@
 import express, { Express } from 'express';
-import { createServer, Server } from 'http';
+import fs from 'fs';
+import { createServer as createHttpsServer, Server as ServerHttps } from 'https';
+import { createServer as createHttpServer, Server as ServerHttp } from 'http';
 import cors from 'cors';
 import socketio from 'socket.io';
+import dotenv from 'dotenv'
+
+const key = fs.readFileSync('./ssl/key.pem');
+const cert = fs.readFileSync('./ssl/cert.pem');
+
+dotenv.config();
 
 class App {
-  private http!: Server;
+  private https!: ServerHttps;
+  private http!: ServerHttp;
   private app: Express;
 
   constructor() {
     this.app = express();
     this.app.use(cors());
+
+    this.app.get('/', (req, res) => {
+      res.send('dale')
+    });
   }
 
   public start() {
-    this.http = createServer(this.app);
+    this.https = createHttpsServer({ key: key, cert: cert }, this.app);
+    this.https.listen(process.env.SSLPORT || 3334, () => {
+      console.log(`https server running on :${process.env.SSLPORT || 3334}`)
+    });
 
+    this.http = createHttpServer(this.app);
     this.http.listen(process.env.PORT || 3333, () => {
-      console.log(`server running on :${process.env.PORT || 3333}`)
+      console.log(`http server running on :${process.env.PORT || 3333}`)
     });
 
     const io = socketio(this.http);
+    const ios = socketio(this.https);
     // setup socket
   }
 }
